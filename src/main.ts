a6dcd005b0eb42317a7452488834cd5d6eeeed59
Iker S.P <ikersierra5@gmail.com>
2023-05-05 01:12:15 +0200

    Added HTTPS to NestJS

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
+import { HttpsOptions } from '@nestjs/common/interfaces/external/https-options.interface';
+
+
+const httpsOptions: HttpsOptions = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
+  passphrase: 'Passw0rd_12345'
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, { httpsOptions, cors: true });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   const configService = app.get(ConfigService);
 
